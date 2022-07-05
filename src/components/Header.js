3af7ede5f026093c1b0b7b84b05ceefe6670e6d1
Nick Danelia <nikdanelia@gmail.com>
2022-07-05 18:05:22 +0400

    inital commit.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/troll-face.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={logo} className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
---
+++ src/components/Meme.js
@@ -0,0 +1,25 @@
+import React from "react";
+import memesData from "../memesData.js";
+
+export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState(memesData.data.memes[0].url);
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={memeImage} className="meme--image" />
+    </main>
+  );
+}
